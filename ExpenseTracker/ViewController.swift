b2a2328788e7df8aa3669893a1283515f431ca7c
Ashwin Kommajesula <ashwinkommajesula@Ashwins-MacBook-Air.local>
2015-09-10 16:44:47 -0400

    Initial Commit

---
+++ ExpenseTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExpenseTracker
+//
+//  Created by Ashwin Kommajesula on 9/10/15.
+//  Copyright (c) 2015 Ashwin Kommajesula. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ExpenseTrackerTests/ExpenseTrackerTests.swift
@@ -0,0 +1,36 @@
+//
+//  ExpenseTrackerTests.swift
+//  ExpenseTrackerTests
+//
+//  Created by Ashwin Kommajesula on 9/10/15.
+//  Copyright (c) 2015 Ashwin Kommajesula. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ExpenseTrackerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
